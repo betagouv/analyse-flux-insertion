2408d456c31cf66cbc3e0c424c4de8c1006f6c92
Thomas Guillet <guillet.thomas@gmail.com>
2021-01-22 13:16:04 +0100

    Permet aux départements de tester leur fichiers CNAF

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,128 @@
+import {useCallback, useEffect, useState} from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const defaultColor = 'white'
+  const [color, setColor] = useState(defaultColor)
+  const [file, setFile] = useState(null)
+  const [total, setTotal] = useState('?')
+  const [countWithEmail, setCountWithEmail] = useState('?')
+  const [countWithUsableEmail, setCountWithUsableEmail] = useState('?')
+  const [hasError, setHasError] = useState(false)
+
+  useEffect(() => {
+    if (!file) {
+      return
+    }
+    const items = new Array(...file.getElementsByTagName('InfoDemandeRSA'))
+    setTotal(items.length)
+
+    const withEmail = items.filter(i => i.getElementsByTagName('ADRELEC').length)
+    setCountWithEmail(withEmail.length)
+
+    const withUsableEmail = items.filter(i => {
+      const ok = i.getElementsByTagName('AUTORUTIADRELEC')[0]
+      return i.getElementsByTagName('ADRELEC').length && ok && ok.innerHTML == "A"
+    })
+    setCountWithUsableEmail(withUsableEmail.length)
+  }, [file])
+
+
+  // useEffect(() => {
+  //   fetch('/test.xml').then(response => {
+  //     const parser = new DOMParser();
+  //     response.text().then(text => {
+  //       const dom = parser.parseFromString(text, "application/xml");
+  //       setFile(dom)
+  //     })
+  //   })
+  // }, [])
+
+  const dragHandler = color => useCallback((event) => {
+    setColor(color)
+    event.preventDefault(); // Prevent file from being open on drop
+  })
+
+  const dropHandler = useCallback((event) => {
+    event.preventDefault();
+
+    setColor(defaultColor)
+    var reader = new FileReader()
+    reader.onload = function(event) {
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(event.target.result, "application/xml");
+      setFile(dom)
+      setHasError(dom.activeElement.nodeName == "parsererror")
+    }
+    reader.readAsText(event.dataTransfer.files[0])
+  })
+
+  const selectHandler = useCallback((event) => {
+    var reader = new FileReader()
+    reader.onload = function(event) {
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(event.target.result, "application/xml");
+      setFile(dom)
+      setHasError(dom.activeElement.nodeName == "parsererror")
+    }
+    reader.readAsText(event.target.files[0])
+  })
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{backgroundColor:color}}>
       <Head>
-        <title>Create Next App</title>
+        <title>Testeur de fichier CNAF</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.main} onDragOver={dragHandler('#0070f3')} onDragLeave={dragHandler(defaultColor)} onDrop={dropHandler}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Testeur de fichier CNAF
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Glissez et déposez le fichier CNAF à analyser ou sélectionnez le.<br/>
+          <input type="file" onChange={selectHandler}/>
         </p>
 
+        <p className={styles.description}>
+          Les opérations sont toutes réalisées sur votre ordinateur.<br/>Aucune donnée n'est transférée.
+        </p>
+
+        {hasError && <div className={styles.grid}>
+          <a href="mailto:data.insertion@beta.gouv.fr?subject=[Flux CNAF]" className={styles.card} style={{borderColor: 'red'}}>
+            <h3>Oups... Une erreur s'est produite.</h3>
+            <p>Essayez-vous d'ouvrir un fichier XML ?</p>
+            <p>Contactez-nous à data.insertion@beta.gouv.fr</p>
+          </a>
+        </div>}
+
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.card}>
+            <h3>{total}</h3>
+            <p>dossiers</p>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.card}>
+            <h3>{countWithEmail}</h3>
+            <p>dossiers avec email</p>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <div className={styles.card}>
+            <h3>{countWithUsableEmail}</h3>
+            <p>dossiers avec email et autorisation de l'utiliser</p>
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="mailto:data.insertion@beta.gouv.fr?subject=[Flux CNAF]" className={styles.card}>
+            <h3></h3>
+            <p>Et si ces personnes pouvaient prendre leur 1er RDV dès aujourd'hui&nbsp;?</p>
           </a>
         </div>
+
+        <p className={styles.description}>
+          Un problème, une question ? Contactez-nous à <a href="mailto:data.insertion@beta.gouv.fr?subject=[Flux CNAF]">data.insertion@beta.gouv.fr</a>
+        </p>
+
       </main>
 
       <footer className={styles.footer}>
@@ -56,8 +131,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Propulsé par data.insertion
         </a>
       </footer>
     </div>
