cc0b26c774913f8ebfe83ddae052dbfe4acb1c7d
Thomas Guillet <guillet.thomas@gmail.com>
2021-02-08 18:31:46 +0100

    Ajoute u nsuivi d'usage anynome avec Matomo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import App from "next/app";
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { init } from "@socialgouv/matomo-next"
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
+
+class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
+  }
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
 }
 
 export default MyApp
