6f2ad1a08263b915fb2085b1684f44e8c8943228
Thomas Guillet <guillet.thomas@gmail.com>
2021-02-15 19:30:49 +0100

    Ajoute des mesures plus fines pour les emails et des infos sur les DSP

--- pages/cnaf/instruction/index.js
+++ pages/cnaf/instruction/index.js
@@ -13,13 +13,27 @@ function init() {
 function reducer(state, action) {
   switch (action.type) {
     case 'append':
-      push(['trackEvent', 'Test - CNAF - Instruction', JSON.stringify(action.item)])
+      let data = {...action.item, seed: undefined}
+      push(['trackEvent', 'Test - CNAF - Instruction', JSON.stringify(data)])
       return [action.item, ...state]
     case 'reset':
       return init()
     default:
       throw new Error()
   }
+}
+
+// TYPEFLUX
+const frequencyNames = {
+  M: 'Mensuel',
+  I: 'Quotidien',
+  R: 'Reprise'
+}
+
+// NATFLUX
+const typeNames = {
+  D: 'Bénéficiaires',
+  I: 'Instructions'
 }
 
 export default function Home() {
@@ -37,11 +51,23 @@ export default function Home() {
     event.preventDefault() // Prevent file from being open on drop
   })
 
+  useEffect(() => {
+    if(file) {
+      fileHandler(file)
+    }
+  }, [file])
+
   const fileHandler = (file) => {
     var reader = new FileReader()
     reader.onload = function(event) {
       const parser = new DOMParser()
       const dom = parser.parseFromString(event.target.result, "application/xml")
+
+      const desc = dom.getElementsByTagName('IdentificationFlux')[0]
+      const frequency = desc.getElementsByTagName('TYPEFLUX')[0].innerHTML
+      const type = desc.getElementsByTagName('NATFLUX')[0].innerHTML
+      const dt = desc.getElementsByTagName('DTCREAFLUX')[0].innerHTML
+      const time = desc.getElementsByTagName('HEUCREAFLUX')[0].innerHTML
 
       const items = new Array(...dom.getElementsByTagName('InfoDemandeRSA'))
       const withEmail = items.filter(i => i.getElementsByTagName('ADRELEC').length)
@@ -49,16 +75,33 @@ export default function Home() {
         const ok = i.getElementsByTagName('AUTORUTIADRELEC')[0]
         return i.getElementsByTagName('ADRELEC').length && ok && ok.innerHTML == "A"
       })
+      const withForbiddenEmailUsage = items.filter(i => {
+        const ok = i.getElementsByTagName('AUTORUTIADRELEC')[0]
+        return i.getElementsByTagName('ADRELEC').length && ok && ok.innerHTML == "R"
+      })
+      const withoutEmailUsage = items.filter(i => {
+        const ok = i.getElementsByTagName('AUTORUTIADRELEC')[0]
+        return i.getElementsByTagName('ADRELEC').length && ok && ok.innerHTML == "I"
+      })
+
+      const withDSP = items.filter(i => i.getElementsByTagName('DonneesSocioProfessionnelles').length)
 
       dispatchRuns({
         type: 'append',
         item: {
+          seed: Math.random(),
           timetamp: (new Date()).toISOString().slice(0,19),
           filename: file.name,
+          fileDatetime: `${dt}-${time}`,
+          frequency,
+          type,
           error: dom.activeElement.nodeName == "parsererror",
           total: items.length,
           withEmail: withEmail.length,
-          withAutorisation: withUsableEmail.length
+          withAutorisation: withUsableEmail.length,
+          withExplicitRefusal: withForbiddenEmailUsage.length,
+          withoutAutorisationDetails: withoutEmailUsage.length,
+          withDSP: withDSP.length
         }
       })
     }
@@ -76,6 +119,7 @@ export default function Home() {
   const selectHandler = useCallback((event) => {
     for (var i = 0; i<event.target.files.length; i++) {
       fileHandler(event.target.files[i])
+      setFile(event.target.files[i])
     }
     event.target.value = ''
   })
@@ -113,17 +157,39 @@ export default function Home() {
               <tr>
                 <th>Date</th>
                 <th>Fichier</th>
+                <th>Date du fichier</th>
+                <th>Fréquence</th>
+                <th>Nature</th>
                 <th>Dossiers</th>
-                <th>avec email</th>
-                <th>et autorisation</th>
+                <th colSpan="2">avec email (%)</th>
+                <th colSpan="2">et autorisation (%)</th>
+                <th colSpan="2">refus explicit (%)</th>
+                <th colSpan="2">inconnu (I) (%)</th>
+                <th colSpan="2">DSP (%)</th>
                 <th>Erreur</th>
               </tr>
-              {runs.map(r => (<tr key={`${r.timetamp}-${r.filename}` }>
+              {runs.map(r => (<tr key={`${r.timetamp}-${r.filename}-${r.seed}` }>
                 <td>{r.timetamp}</td>
+                <td>{r.fileDatetime}</td>
                 <td>{r.filename}</td>
+                <td>{`${r.frequency} (${frequencyNames[r.frequency] || '?'})`}</td>
+                <td>{`${r.type} (${typeNames[r.type] || '?'})`}</td>
                 <td className={styles.numeric}>{r.total}</td>
-                <td className={styles.numeric}>{r.withEmail} ({round(r.withEmail/r.total*100)}%)</td>
-                <td className={styles.numeric}>{r.withAutorisation} ({round(r.withAutorisation/r.total*100)}%)</td>
+
+                <td className={styles.numeric}>{r.withEmail}</td>
+                <td className="shrink">{round(r.withEmail/r.total*100)}</td>
+
+                <td className={styles.numeric}>{r.withAutorisation}</td>
+                <td className="shrink">{round(r.withAutorisation/r.total*100)}</td>
+
+                <td className={styles.numeric}>{r.withExplicitRefusal}</td>
+                <td className="shrink">{round(r.withExplicitRefusal/r.total*100)}</td>
+
+                <td className={styles.numeric}>{r.withoutAutorisationDetails}</td>
+                <td className="shrink">{round(r.withoutAutorisationDetails/r.total*100)}</td>
+
+                <td className={styles.numeric}>{r.withDSP}</td>
+                <td className="shrink">{round(r.withDSP/r.total*100)}</td>
                 <td>{r.error ? 'Oui' : 'Non'}</td>
               </tr>
             ))}
