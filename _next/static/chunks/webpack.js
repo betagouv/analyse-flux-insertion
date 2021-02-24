a8d054e6129c362f38e9681706c3e1207e38897f
Thomas Guillet <guillet.thomas@gmail.com>
2021-02-24 18:41:07 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0226af536636ab457d03";
+/******/ 	var hotCurrentHash = "e99eb18aa6dab342690d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
