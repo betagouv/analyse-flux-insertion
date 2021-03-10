580486b92aa8ec76c0da452314978ab98c0d9df2
Quentin Blanc <qblanc@hotmail.com>
2021-03-10 17:35:03 +0100

    create user form & css

--- pages/create-user.js
+++ pages/create-user.js
@@ -1,11 +1,28 @@
 // Ajouter une page qui prend 2 champs et un bouton.
 // Quand tu cliques sur le bouton, les 2 champs sont enregistrés dans une boite, et display ailleurs sur la page.
-import React from 'react'
-import Link from 'next/link'
+import { useState } from 'react'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 
 export default function CreateUser() {
+  const [userFirstName, setUserFirstName] = useState('');
+  const [userLastName, setUserLastName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = { first_name: userFirstName, last_name: userLastName };
+    console.log(user);
+    alert(`Fiche utilisateur créée sur RDV Solidarités avec succès pour ${userFirstName} ${userLastName}`);
+    // fetch('https://www.rdv-solidarites.fr/api/v1/users', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify(user)
+    // }).then(() => {
+    //   setUserFirstName('');
+    //   setUserLastName('');
+    //   alert("Fiche utilisateur créée sur RDV Solidarités avec succès");
+    // })
+  }
 
   return(
     <Layout className={styles.container}>
@@ -13,6 +30,27 @@ export default function CreateUser() {
         <h1 className={styles.title}>
           Créer une fiche bénéficiaire sur RDV Solidarités
         </h1>
+        <div className={styles.create}>
+          <form
+            onSubmit={handleSubmit}
+          >
+            <label>Prénom</label>
+            <input
+              type="text"
+              value={userFirstName}
+              onChange={(e) => setUserFirstName(e.target.value)}
+              required
+            />
+            <label>Nom</label>
+            <input
+              type="text"
+              value={userLastName}
+              onChange={(e) => setUserLastName(e.target.value)}
+              required
+            />
+            <button>Créer fiche utilisateur</button>
+          </form>
+        </div>
 
       </main>
     </Layout>
