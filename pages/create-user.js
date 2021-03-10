50485d54c5d4ccd8e5aa514eefdeee8fa4a7c785
Quentin Blanc <qblanc@hotmail.com>
2021-03-10 17:46:43 +0100

    delete unnecessary comments

--- pages/create-user.js
+++ pages/create-user.js
@@ -1,5 +1,3 @@
-// Ajouter une page qui prend 2 champs et un bouton.
-// Quand tu cliques sur le bouton, les 2 champs sont enregistrés dans une boite, et display ailleurs sur la page.
 import { useState } from 'react'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
