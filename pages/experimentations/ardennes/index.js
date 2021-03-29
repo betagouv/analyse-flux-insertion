90c3ac3e6a0f7c8b1e27076265069977f8341a7f
Quentin Blanc <qblanc@hotmail.com>
2021-03-29 17:07:15 +0200

    lecture et écriture du fichier ok

--- pages/experimentations/ardennes/index.js
+++ pages/experimentations/ardennes/index.js
@@ -2,7 +2,6 @@
 // Simplifier le travail d'Isabelle. More details to come...
 
 import {useCallback, useEffect, useState, useReducer} from 'react'
-import Head from 'next/head'
 import * as XLSX from 'xlsx';
 
 // import Admin from '../../../components/admin'
@@ -12,6 +11,7 @@ import Mailer from '../../../components/mailer'
 import PendingMessage from '../../../components/pending'
 import styles from '../../../styles/Home.module.css'
 
+import { getFormattedTime } from '../../../lib/cnaf'
 import { initReducer, reducerFactory } from '../../../lib/historique'
 
 const reducer = reducerFactory('Expérimentation Ardennes - CNAF - Bénéficiaire')
@@ -19,16 +19,10 @@ const devMode = process.env.NODE_ENV == 'development'
 
 export default function Ardennes() {
   const [devFile, setDevFile] = useState(null)
+  const [parsedData, setParsedData] = useState([]);
   const [runs, dispatchRuns] = useReducer(reducer, [], initReducer)
   const [isPending, setIsPending] = useState(false);
   const [fileSize, setFileSize] = useState(0);
-
-  const handleNewRuns = useCallback(data => {
-    dispatchRuns({
-      type: 'reset',
-      items: data
-    })
-  })
 
   useEffect(() => {
     if(devFile) {
@@ -63,37 +57,52 @@ export default function Ardennes() {
       range.e.c = 19; // 19 == XLSX.utils.decode_col("T")
       const new_range = XLSX.utils.encode_range(range);
       /* Convert array to json*/
-      const dataParse = XLSX.utils.sheet_to_json(worksheet, {blankrows: false, raw: false, range: new_range});
-      console.log(dataParse);
+      const dataParse = XLSX.utils.sheet_to_json(worksheet, { blankrows: false, raw: false, defval: null, range: new_range });
 
+      setParsedData(
+        dataParse.map((user, index) => {
+          if (user["Compte rdv"] != "O") {
+            // (pour plus tard) Appeller l'API pour vérifier la présence de l'utilisateur
+            // Créer l'utilisateur dans RDV Solidarités
+            // Vérifier succès, then
+            user["Compte rdv"] = "O";
+            user["RDV pris"] = "N";
+            return user;
+            // Récupérer le token d'invitation
+            // Envoyer le mail à la place d'Isabelle
+          } else if (user["RDV pris"] == "O") {
+            // Vérifier si l'utilisateur a pris un rdv
+            // si oui => user["RDV pris"] = "O";
+            // si non => user["RDV pris"] = "N";
+            return user;
+          }
+        })
+      );
+
+      const outWorkbook = XLSX.utils.book_new();
+      const outWorksheet = XLSX.utils.json_to_sheet(parsedData);
+      XLSX.utils.book_append_sheet(outWorkbook, outWorksheet, "DIVERGENCES");
+      XLSX.utils.book_append_sheet(outWorkbook, xls.Sheets[xls.SheetNames[1]], "GRAPHIQUE");
+      XLSX.utils.book_append_sheet(outWorkbook, xls.Sheets[xls.SheetNames[2]], "Selection COA");
+      XLSX.writeFile(outWorkbook, `ardennes_rsa_${getFormattedTime()}.xlsx`)
 
       setIsPending(false);
 
-      // dispatchRuns({
-      //   type: 'append',
-      //   item: {
-      //     seed: Math.random(),
-      //     timestamp: (new Date()).toISOString().slice(0, 19),
-      //     duration: (new Date()) - start_time,
-      //     filename: file.name,
-      //     fileDatetime: `${dt}-${time}`,
-      //     frequency,
-      //     type,
-      //     // WIP: OK sur Firefox KO sur Chrome
-      //     error: dom.activeElement && dom.activeElement.nodeName == "parsererror",
-      //     total: items.length,
-      //     fileSize: file.size,
-      //     dates,
-      //   }
-      // })
+      dispatchRuns({
+        type: 'append',
+        item: {
+          timestamp: (new Date()).toISOString().slice(0, 19),
+          duration: (new Date()) - start_time,
+          filename: file.name,
+          fileSize: file.size,
+        }
+      })
     }
     reader.readAsArrayBuffer(file);
   }
 
-  const round = (value) => Math.round(value)
   return (
     <Layout className={styles.container}>
-      {/* <Admin category="Bénéficiaire" onRunRefresh={handleNewRuns}/> */}
       <main className={styles.main}>
         <h1 className={styles.title}>
           Expérimentation Ardennes
@@ -115,67 +124,41 @@ export default function Ardennes() {
           <a href="#pourquoi">Pourquoi un tel lecteur&nbsp;?</a>
         </p>
 
-{/* Tableau de résultat */}
-        {/* { runs && runs.length > 0 && (<>
+        { parsedData && (<>
           <h2 className={styles.subtitle}>
-            Historique
+            Bénéficiaires RSA
           </h2>
 
-          <table>
-            <thead>
-              <tr>
-                <th rowSpan="2">Date</th>
-                <th rowSpan="2">Fichier</th>
-                { devMode && <th rowSpan="2">Taille</th>}
-                { devMode && <th rowSpan="2">Durée<br/>(en s)</th>}
-                <th rowSpan="2">Date du fichier</th>
-                <th rowSpan="2">Fréquence</th>
-                <th rowSpan="2">Nature</th>
-                <th rowSpan="2">Dossiers</th>
-                <th rowSpan="2">Détails</th>
-                <th rowSpan="2">Erreur</th>
-              </tr>
-              <tr>
-              </tr>
-            </thead>
-            <tbody>
-              {runs.map((r, i) => (<tr key={`${r.timestamp}-${r.filename}-${r.seed}` } style={ i == dateData.index ? {backgroundColor: 'lightgrey'}: {} }>
-                <td>{r.timestamp}</td>
-                <td>{r.filename}</td>
-                { devMode && <td>{r.fileSize}</td>}
-                { devMode && <td>{!isNaN(r.duration) ? r.duration/1000 : "#N/A"}</td>}
-                <td>{r.fileDatetime}</td>
-                <td>{`${r.frequency} (${frequencyNames[r.frequency] || '?'})`}</td>
-                <td>{`${r.type} (${typeNames[r.type] || '?'})`}</td>
-                <td className={styles.numeric}>{r.total}</td>
-                <td className="shrink"><button onClick={handleDateHistogram} data-index={i}>Afficher par date</button></td>
-                <td>{r.error ? 'Oui' : 'Non'}</td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
-
-          <button onClick={() => dispatchRuns({type: 'reset'})}>Vider l'historique</button>
-        </>)} */}
-
-{/* Calendrier */}
-        {/* {runs.length != 0 && (<p className={styles.text}>
-        Vous pouvez accéder à une représentation graphique de la répartition dans les temps des demandes connues dans les fichiers analysés. Pour cela il faut cliquer sur le bouton « Afficher par date ».
-          </p>)}
-
-        {dateData.index != undefined && (
-          <>
-            <h2 className={styles.subtitle}>
-              Nombre de dossiers associés à des demandes réalisés à chaque date
-            </h2>
-            <div style={{height: (250*dateData.yearCount) + "px", width: "100%"}}>
-              {ResponsiveCalendar(dateData)}
-            </div>
-          </>
-        )} */}
+          {parsedData.length == 0 &&
+            <p className={styles.description}>Aucun bénéficiaire dans le fichier</p>
+          }
+          { parsedData.length > 0 &&
+            <table>
+              <thead>
+                <tr>
+                  <th rowSpan="2">Nom</th>
+                  <th rowSpan="2">Prénom</th>
+                  <th rowSpan="2">Mail</th>
+                  <th rowSpan="2">Téléphone</th>
+                </tr>
+                <tr>
+                </tr>
+              </thead>
+              <tbody>
+                {parsedData.map((user, index) => (<tr key={index}>
+                  <td>{user["NOM"]}</td>
+                  <td>{user["PRENOM"]}</td>
+                  <td>{user["MAIL"]}</td>
+                  <td>{user["TELEPHONE"]}</td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
+          }
+        </>)}
 
         <p className={styles.description}>
-          Un problème, une question ? Contactez-nous à <Mailer subject="Flux bénéficiaire CNAF">data.insertion@beta.gouv.fr</Mailer>
+          Un problème, une question ? Contactez-nous à <Mailer subject="Expérimentation Ardennes">data.insertion@beta.gouv.fr</Mailer>
         </p>
 
         <h2 id="pourquoi" className={styles.subtitle}>
