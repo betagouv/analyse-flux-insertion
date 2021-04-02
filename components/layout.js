e7974949e73cd189ec186fdfb7353c240260373b
Quentin Blanc <qblanc@hotmail.com>
2021-04-03 00:36:54 +0200

    léger style pour boutons

--- components/layout.js
+++ components/layout.js
@@ -33,7 +33,6 @@ export default function Layout({
       onDrop={dropHandler}>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
