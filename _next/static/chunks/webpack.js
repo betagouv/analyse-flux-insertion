ea8e2a80918f2f6a15df6153b9c39aa43f304f7c
aminedhobb <amine.dhobb@gmail.com>
2021-05-11 10:40:28 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0110ac760135fd35f980";
+/******/ 	var hotCurrentHash = "79d42a4f04ba8c40b7aa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
