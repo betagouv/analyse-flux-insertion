62867dd2f6a0565572f55061587bc2a8d9d9ea25
Quentin Blanc <qblanc@hotmail.com>
2021-05-31 17:16:55 +0200

    refacto

--- pages/experimentations/ardennes-demo/index.js
+++ pages/experimentations/ardennes-demo/index.js
@@ -105,9 +105,7 @@ export default function Ardennes() {
 
   async function createUser(userData, userIndex) {
     const address = userData["ADRESSE"] + " - " + userData["CODE POSTAL"] + " " + userData["VILLE"];
-    userData["N°CAF"].trim().length == 13 || userData["N°CAF"].trim().length == 15
-      ? userData["N°CAF"] = ""
-      : userData["N°CAF"] = userData["N°CAF"];
+    if ([13, 15].includes(userData["N°CAF"].trim().length)) { userData["N°CAF"] = "" }
 
     let user = {
       first_name: userData["PRENOM"],
