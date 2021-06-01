dcfc47ebf8b66384852e9e4156f16bbc82e8f6fc
Quentin Blanc <qblanc@hotmail.com>
2021-06-01 16:02:43 +0200

    bracketless style

--- pages/experimentations/ardennes-demo/index.js
+++ pages/experimentations/ardennes-demo/index.js
@@ -105,7 +105,7 @@ export default function Ardennes() {
 
   async function createUser(userData, userIndex) {
     const address = userData["ADRESSE"] + " - " + userData["CODE POSTAL"] + " " + userData["VILLE"];
-    if ([13, 15].includes(userData["N°CAF"].trim().length)) { userData["N°CAF"] = "" }
+    if ([13, 15].includes(userData["N°CAF"].trim().length)) userData["N°CAF"] = ""
 
     let user = {
       first_name: userData["PRENOM"],
